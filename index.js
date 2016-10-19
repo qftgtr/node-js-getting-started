e70defaabc8ed2c29b135316cc07a0d9d6a075fa
qftgtr <qft.gtr@gmail.com>
2016-10-20 00:08:23 +0800

    contact us

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var https = require('https');
+var querystring = require('querystring');
+
 var express = require('express');
 var app = express();
 
@@ -10,8 +13,28 @@ app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
 app.get('/subscribe', function(request, response) {
-  response.send('pages/index.html');
-//  response.render('pages/index.html');
+  var data = querystring.stringify({
+    'name': 'web',
+    'company': 'web',
+    'email': request.query.email,
+    'from': 'web',
+  });
+
+  var options = {
+    protocol: 'https:',
+    host: 'admin.officewell.co',
+    path: '/api/v1/feedback',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+
+  var req = https.request(options);
+  req.write(data);
+  req.end();
+  response.send('success');
 });
 
 app.listen(app.get('port'), function() {
